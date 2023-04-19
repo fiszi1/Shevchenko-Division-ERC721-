b4cab19924b2b1881eec7ceb8e9c05f9f56be8af
imuns3228 <imunus3228@gmail.com>
2023-04-19 13:37:19 +0300

    erc721

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -4,16 +4,17 @@ import * as Marketplaces from '../lib/Marketplaces';
 import whitelistAddresses from './whitelist.json';
 
 const CollectionConfig: CollectionConfigInterface = {
-  testnet: Networks.ethereumTestnet,
-  mainnet: Networks.ethereumMainnet,
+  testnet: Networks.polygonTestnet,
+  mainnet: Networks.polygonMainnet,
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'Shevchenko',
+  tokenName: 'SHEVCHENKO_DIVISION',
+  tokenSymbol: 'SVCHNK',
+  hiddenMetadataUri:
+    "ipfs://Qmb7TDAKhtidGUE85LRntioMhm7WGS1ZiFsjx59DnWrRK1/metadata.json",
+  maxSupply: 8,
   whitelistSale: {
     price: 0.05,
     maxMintAmountPerTx: 1,
@@ -26,8 +27,8 @@ const CollectionConfig: CollectionConfigInterface = {
     price: 0.09,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  contractAddress: "0x8fF376e4AEBE1A9B19150d7f31666785C94774Ef",
+  marketplaceIdentifier: 'Shevchenko division',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
